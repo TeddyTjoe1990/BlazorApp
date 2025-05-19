b4a7310aac8fbc6d716832504dc57f2775e92673
TeddyTjoe1990 <her21059@byui.edu>
2025-05-19 18:44:08 +0700

    image size was updated

--- BlazingPizza/obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
+++ BlazingPizza/obj/Debug/net9.0/BlazingPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22f394e857afff677d561970f898ab039ec4005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d5e463c4edd000e2ce859241748abd8cc7aff9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
